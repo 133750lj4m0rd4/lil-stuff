297a830fcd44bd1ae754ec0a4aecc0789186156c
ME <you@example.com>
2023-12-11 16:20:07 +0500

    updated nets

--- rainbow_nets.py
+++ rainbow_nets.py
@@ -2,36 +2,45 @@ import turtle
 
 colors = [
     (1,0,0),
-    (0,1,0),
-    (0,0,1),
+    (1,0.5,0),
     (1,1,0),
-    (1,0,1),
-    (0,1,1),
+    (0,1,0),
+    (0,0.75,0.75),
+    (0,0,1),
+    (0.75,0,0.75),
 ]
+step = 35
+cycles = 11
+angle = 180/cycles
+squares = 51
 
 turtle.bgcolor(0,0,0)
-turtle.pensize(3)
+turtle.pensize(1)
 turtle.speed(0)
+turtle.tracer(0)
 turtle.penup()
 
-for i in range(6):
-    turtle.color(colors[i])
+for i in range(cycles):
+    turtle.color(colors[i%7])
 
-    turtle.backward(400)
+    turtle.backward(step*squares/2)
     turtle.right(90)
-    turtle.backward(400)
+    turtle.backward(step*squares/2)
     turtle.left(90)
 
-    for i in range(-16,17):
+    for i in range(int(-squares/2),int(squares/2)+(squares%2)+1):
         turtle.pendown()
-        turtle.forward(800)
+        turtle.forward(step*squares)
         turtle.penup()
-        turtle.backward(800)
+        turtle.backward(step*squares)
         turtle.right(90)
-        turtle.forward(25)
+        turtle.forward(step)
         turtle.left(90)
+        turtle.update()
 
     turtle.setpos(0,0)
-    turtle.right(30)
+    turtle.right(angle)
+
+turtle.setpos(-10000,-10000)
 
 while True: turtle.update()
