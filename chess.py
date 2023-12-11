78f4e35da06e6eb707e26da2ae4d4ed4a58d12d7
MORDA <m0rd4w0rk5tuff@gmail.com>
2023-12-11 12:13:03 +0500

    pawn moves thingy idk how to explain

--- chess.py
+++ chess.py
@@ -1,48 +1,60 @@
+pawn_moves = {
+    #(is_white,on_first_rank):(vec,vec)
+    (True,True):((-1,0),(-2,0)),
+    (True,False):((-1,0)),
+    (False,True):((1,0),(2,0)),
+    (False,False):((1,0)),
+}
+
 class figure:
     def __init__(self,is_white:bool,type:chr,position:tuple[int]):
         self.is_white = is_white
         self.type = type
         self.position = position
+    
+    def posible_moves(self):
+        temp = (1,6)
+        return(pawn_moves[(self.is_white,self.position[0] == temp[self.is_white])])
 
 
 class board():
     def __init__(self):
         self.figures = {
-            figure(True,'R',(0,0)),
-            figure(True,'k',(0,1)),
-            figure(True,'b',(0,2)),
-            figure(True,'Q',(0,3)),
-            figure(True,'K',(0,4)),
-            figure(True,'b',(0,5)),
-            figure(True,'k',(0,6)),
-            figure(True,'R',(0,7)),
+            figure(False,'R',(0,0)),
+            figure(False,'k',(0,1)),
+            figure(False,'b',(0,2)),
+            figure(False,'Q',(0,3)),
+            figure(False,'K',(0,4)),
+            figure(False,'b',(0,5)),
+            figure(False,'k',(0,6)),
+            figure(False,'R',(0,7)),
 
-            figure(True,'p',(1,0)),
-            figure(True,'p',(1,1)),
-            figure(True,'p',(1,2)),
-            figure(True,'p',(1,3)),
-            figure(True,'p',(1,4)),
-            figure(True,'p',(1,5)),
-            figure(True,'p',(1,6)),
-            figure(True,'p',(1,7)),
+            figure(False,'p',(1,0)),
+            figure(False,'p',(1,1)),
+            figure(False,'p',(1,2)),
+            figure(False,'p',(1,3)),
+            figure(False,'p',(1,4)),
+            figure(False,'p',(1,5)),
+            figure(False,'p',(1,6)),
+            figure(False,'p',(1,7)),
             
-            figure(False,'R',(7,0)),
-            figure(False,'k',(7,1)),
-            figure(False,'b',(7,2)),
-            figure(False,'Q',(7,3)),
-            figure(False,'K',(7,4)),
-            figure(False,'b',(7,5)),
-            figure(False,'k',(7,6)),
-            figure(False,'R',(7,7)),
+            figure(True,'R',(7,0)),
+            figure(True,'k',(7,1)),
+            figure(True,'b',(7,2)),
+            figure(True,'Q',(7,3)),
+            figure(True,'K',(7,4)),
+            figure(True,'b',(7,5)),
+            figure(True,'k',(7,6)),
+            figure(True,'R',(7,7)),
 
-            figure(False,'p',(6,0)),
-            figure(False,'p',(6,1)),
-            figure(False,'p',(6,2)),
-            figure(False,'p',(6,3)),
-            figure(False,'p',(6,4)),
-            figure(False,'p',(6,5)),
-            figure(False,'p',(6,6)),
-            figure(False,'p',(6,7)),
+            figure(True,'p',(6,0)),
+            figure(True,'p',(6,1)),
+            figure(True,'p',(6,2)),
+            figure(True,'p',(6,3)),
+            figure(True,'p',(6,4)),
+            figure(True,'p',(6,5)),
+            figure(True,'p',(6,6)),
+            figure(True,'p',(6,7)),
         }
         self.board = []
         self.update_board()
